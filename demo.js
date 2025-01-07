55284c1ce597917e1761e8a7d51fe92d301c2457
Bruno Frasan <brunofrancco@gmail.com>
2025-01-07 15:43:22 +0000

    first commit

---
+++ demo.js
@@ -0,0 +1,21 @@
+// import prisma from './db/index.js'
+const prisma = require('./db/index')
+
+const newBook = {
+	title: 'The Fellowship of the Ring',
+	year: 1954,
+	quantity: 5,
+	genre: ['High fantasy', 'Adventure'],
+	authorName: 'J. R. R. Tolkien',
+}
+
+prisma.book
+	.create({ data: newBook })
+	.then((book) => {
+		console.log(`Success... a new book was created!`)
+		console.log(book)
+	})
+	.catch((error) => {
+		console.log(`Semething went wrong!!`)
+		console.log(error)
+	})
